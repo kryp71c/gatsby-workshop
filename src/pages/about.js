8fa0a6df39e7012b80de4d24365fd19ed8373848
ChrisH <cfh2120@cumc.columbia.edu>
2021-03-03 10:18:09 -0800

    Add resource links to about us page

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,15 @@ import * as React from "react"
 
 const AboutPage = () => {
   return (
+    <main>
+    <title>About this site</title>
     <h1>About this site</h1>
+    <p>This site was created during gatsby conf 2021</p>
+    <ul>
+      <li><a href="https://intro-workshop-gatsbyconf-2021.gatsbyjs.io/">Workshop Curriculum</a></li>
+      <li><a href="http://bit.ly/3e4LCnY">Slides</a></li>
+    </ul>
+    </main>
   )
 }
 
